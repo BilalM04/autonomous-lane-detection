5391c7d71093926d72b7b1002c29108fccdf4324
Mohammad Bilal <bilal71612@gmail.com>
2024-09-09 20:37:40 -0400

    initial commit

---
+++ LaneLines.py
@@ -0,0 +1,316 @@
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+
+def hist(img):
+    bottom_half = img[img.shape[0]//2:,:]
+    return np.sum(bottom_half, axis=0)
+
+class LaneLines:
+    """ Class containing information about detected lane lines.
+
+    Attributes:
+        left_fit (np.array): Coefficients of a polynomial that fit left lane line
+        right_fit (np.array): Coefficients of a polynomial that fit right lane line
+        parameters (dict): Dictionary containing all parameters needed for the pipeline
+        debug (boolean): Flag for debug/normal mode
+    """
+    def __init__(self):
+        """Init Lanelines.
+
+        Parameters:
+            left_fit (np.array): Coefficients of polynomial that fit left lane
+            right_fit (np.array): Coefficients of polynomial that fit right lane
+            binary (np.array): binary image
+        """
+        self.left_fit = None
+        self.right_fit = None
+        self.binary = None
+        self.nonzero = None
+        self.nonzerox = None
+        self.nonzeroy = None
+        self.clear_visibility = True
+        self.dir = []
+        self.left_curve_img = mpimg.imread('./display_images/left_turn.png')
+        self.right_curve_img = mpimg.imread('./display_images/right_turn.png')
+        self.keep_straight_img = mpimg.imread('./display_images/straight.png')
+        self.left_curve_img = cv2.normalize(src=self.left_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.right_curve_img = cv2.normalize(src=self.right_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.keep_straight_img = cv2.normalize(src=self.keep_straight_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # HYPERPARAMETERS
+        # Number of sliding windows
+        self.nwindows = 9
+        # Width of the the windows +/- margin
+        self.margin = 100
+        # Mininum number of pixels found to recenter window
+        self.minpix = 50
+
+    def draw_lanes(self, img):
+        """Take a image and detect lane lines.
+
+        Parameters:
+            img (np.array): An binary image containing relevant pixels
+
+        Returns:
+            Image (np.array): An RGB image containing lane lines pixels and other details
+        """
+        self.extract_features(img)
+        return self.detect_lanes(img)
+    
+    def draw_sliding_windows(self, img):
+        """Take a image and draw sliding windows.
+
+        Parameters:
+            img (np.array): An binary image containing relevant pixels
+
+        Returns:
+            Image (np.array): A GRAY image containing sliding windows.
+        """
+        self.extract_features(img)
+        return self.sliding_window(img)
+
+    def pixels_in_window(self, center, margin, height):
+        """ Return all pixel that in a specific window
+
+        Parameters:
+            center (tuple): coordinate of the center of the window
+            margin (int): half width of the window
+            height (int): height of the window
+
+        Returns:
+            pixelx (np.array): x coordinates of pixels that lie inside the window
+            pixely (np.array): y coordinates of pixels that lie inside the window
+        """
+        topleft = (center[0]-margin, center[1]-height//2)
+        bottomright = (center[0]+margin, center[1]+height//2)
+
+        condx = (topleft[0] <= self.nonzerox) & (self.nonzerox <= bottomright[0])
+        condy = (topleft[1] <= self.nonzeroy) & (self.nonzeroy <= bottomright[1])
+        return self.nonzerox[condx&condy], self.nonzeroy[condx&condy]
+
+    def extract_features(self, img):
+        """ Extract features from a binary image
+
+        Parameters:
+            img (np.array): A binary image
+        """
+        self.img = img
+        # Height of of windows - based on nwindows and image shape
+        self.window_height = int(img.shape[0]//self.nwindows)
+
+        # Identify the x and y positions of all nonzero pixel in the image
+        self.nonzero = img.nonzero()
+        self.nonzerox = np.array(self.nonzero[1])
+        self.nonzeroy = np.array(self.nonzero[0])
+
+    def find_lane_pixels(self, img):
+        """Find lane pixels from a binary warped image.
+
+        Parameters:
+            img (np.array): A binary warped image
+
+        Returns:
+            leftx (np.array): x coordinates of left lane pixels
+            lefty (np.array): y coordinates of left lane pixels
+            rightx (np.array): x coordinates of right lane pixels
+            righty (np.array): y coordinates of right lane pixels
+            out_img (np.array): A RGB image that use to display result later on.
+            window_centroids (list): List of window positions for visualization
+        """
+        assert(len(img.shape) == 2)
+
+        # Create an output image to draw on and visualize the result
+        out_img = np.dstack((img, img, img))
+
+        histogram = hist(img)
+        midpoint = histogram.shape[0] // 2
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Current position to be updated later for each window in nwindows
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        y_current = img.shape[0] + self.window_height // 2
+
+        # Create empty lists to receive left and right lane pixel
+        leftx, lefty, rightx, righty = [], [], [], []
+
+        # List to store window positions for visualization
+        window_centroids = []
+
+        # Step through the windows one by one
+        for _ in range(self.nwindows):
+            y_current -= self.window_height
+            center_left = (leftx_current, y_current)
+            center_right = (rightx_current, y_current)
+
+            good_left_x, good_left_y = self.pixels_in_window(center_left, self.margin, self.window_height)
+            good_right_x, good_right_y = self.pixels_in_window(center_right, self.margin, self.window_height)
+
+            # Append these indices to the lists
+            leftx.extend(good_left_x)
+            lefty.extend(good_left_y)
+            rightx.extend(good_right_x)
+            righty.extend(good_right_y)
+
+            # Append window positions for visualization
+            window_centroids.append((leftx_current, rightx_current))
+
+            if len(good_left_x) > self.minpix:
+                leftx_current = np.int32(np.mean(good_left_x))
+            if len(good_right_x) > self.minpix:
+                rightx_current = np.int32(np.mean(good_right_x))
+
+        return leftx, lefty, rightx, righty, out_img, window_centroids
+
+
+    def detect_lanes(self, img):
+        """Find the lane line from an image and draw it.
+
+        Parameters:
+            img (np.array): a binary warped image
+
+        Returns:
+            out_img (np.array): a RGB image that have lane line drawn on that.
+        """
+
+        leftx, lefty, rightx, righty, out_img, _ = self.find_lane_pixels(img)
+
+        if len(lefty) > 1500:
+            self.left_fit = np.polyfit(lefty, leftx, 2)
+        if len(righty) > 1500:
+            self.right_fit = np.polyfit(righty, rightx, 2)
+
+        # Generate x and y values for plotting
+        maxy = img.shape[0] - 1
+        miny = img.shape[0] // 3
+        if len(lefty):
+            maxy = max(maxy, np.max(lefty))
+            miny = min(miny, np.min(lefty))
+
+        if len(righty):
+            maxy = max(maxy, np.max(righty))
+            miny = min(miny, np.min(righty))
+
+        ploty = np.linspace(miny, maxy, img.shape[0])
+
+        left_fitx = self.left_fit[0]*ploty**2 + self.left_fit[1]*ploty + self.left_fit[2]
+        right_fitx = self.right_fit[0]*ploty**2 + self.right_fit[1]*ploty + self.right_fit[2]
+
+        # Visualization
+        for i, y in enumerate(ploty):
+            l = int(left_fitx[i])
+            r = int(right_fitx[i])
+            y = int(y)
+            cv2.line(out_img, (l, y), (r, y), (0, 255, 0))
+
+        return out_img
+    
+    def sliding_window(self, img):
+        """Find the sliding windows and draw it.
+
+        Parameters:
+            img (np.array): a binary warped image
+
+        Returns:
+            out_img (np.array): a GRAY image with sliding windows drawn on it.
+        """
+
+        _, _, _, _, out_img, window_centroids = self.find_lane_pixels(img)
+
+        # Draw sliding window boxes
+        for level, (left_center, right_center) in enumerate(window_centroids):
+            win_y_low = img.shape[0] - (level + 1) * self.window_height
+            win_y_high = img.shape[0] - level * self.window_height
+            win_xleft_low = left_center - self.margin
+            win_xleft_high = left_center + self.margin
+            win_xright_low = right_center - self.margin
+            win_xright_high = right_center + self.margin
+
+            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (255, 255, 255), 2)
+            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (255, 255, 255), 2)
+
+        return out_img
+
+    def plot(self, out_img):
+        np.set_printoptions(precision=6, suppress=True)
+        lR, rR, pos = self.measure_curvature()
+
+        value = None
+        if abs(self.left_fit[0]) > abs(self.right_fit[0]):
+            value = self.left_fit[0]
+        else:
+            value = self.right_fit[0]
+
+        if abs(value) <= 0.00015:
+            self.dir.append('F')
+        elif value < 0:
+            self.dir.append('L')
+        else:
+            self.dir.append('R')
+        
+        if len(self.dir) > 10:
+            self.dir.pop(0)
+
+        W = 400
+        H = 500
+        widget = np.copy(out_img[:H, :W])
+        widget //= 2
+        out_img[:H, :W] = widget
+
+        direction = max(set(self.dir), key = self.dir.count)
+        msg = "Keep Straight Ahead"
+        curvature_msg = "Curvature = {:.0f} m".format(min(lR, rR))
+        if direction == 'L':
+            y, x = self.left_curve_img[:,:,3].nonzero()
+            out_img[y, x-100+W//2] = self.left_curve_img[y, x, :3]
+            msg = "Left Curve Ahead"
+        if direction == 'R':
+            y, x = self.right_curve_img[:,:,3].nonzero()
+            out_img[y, x-100+W//2] = self.right_curve_img[y, x, :3]
+            msg = "Right Curve Ahead"
+        if direction == 'F':
+            y, x = self.keep_straight_img[:,:,3].nonzero()
+            out_img[y, x-100+W//2] = self.keep_straight_img[y, x, :3]
+
+        cv2.putText(out_img, msg, org=(10, 240), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+        if direction in 'LR':
+            cv2.putText(out_img, curvature_msg, org=(10, 280), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+        cv2.putText(
+            out_img,
+            "Good Lane Keeping",
+            org=(10, 400),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1.2,
+            color=(0, 255, 0),
+            thickness=2)
+
+        cv2.putText(
+            out_img,
+            "Vehicle is {:.2f} m away from center".format(pos),
+            org=(10, 450),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.6,
+            color=(255, 255, 255),
+            thickness=2)
+
+        return out_img
+
+    def measure_curvature(self):
+        ym = 30/720
+        xm = 3.7/700
+
+        left_fit = self.left_fit.copy()
+        right_fit = self.right_fit.copy()
+        y_eval = 700 * ym
+
+        # Compute R_curve (radius of curvature)
+        left_curveR =  ((1 + (2*left_fit[0] *y_eval + left_fit[1])**2)**1.5)  / np.absolute(2*left_fit[0])
+        right_curveR = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+
+        xl = np.dot(self.left_fit, [700**2, 700, 1])
+        xr = np.dot(self.right_fit, [700**2, 700, 1])
+        pos = (1280//2 - (xl+xr)//2)*xm
+        return left_curveR, right_curveR, pos 
---
+++ main.py
@@ -0,0 +1,110 @@
+"""
+Autonomous Lane Detection
+
+Usage:
+    main.py [--video --image] [--all] INPUT_PATH 
+
+Options:
+
+-h --help                               show this screen
+--video                                 process video file
+--image                                 process image file
+--all                                   output all results (complete, sliding window, and bird's eye)
+"""
+
+import numpy as np
+import matplotlib.image as mpimg
+import cv2
+from docopt import docopt
+from moviepy.editor import VideoFileClip
+from Thresholding import *
+from PerspectiveTransformation import *
+from LaneLines import *
+import os
+
+class FindLaneLines:
+    def __init__(self):
+        """ Init Application"""
+        self.thresholding = Thresholding()
+        self.transform = PerspectiveTransformation()
+        self.lanelines = LaneLines()
+
+    def sliding_window(self, img):
+        out_img = np.copy(img)
+        out_img = self.transform.forward(out_img)
+        out_img = self.thresholding.forward(out_img)
+        out_img = self.lanelines.draw_sliding_windows(out_img)
+        return out_img
+    
+    def complete(self, img):
+        out_img = np.copy(img)
+        img = self.transform.forward(img)
+        img = self.thresholding.forward(img)
+        img = self.lanelines.draw_lanes(img)
+        img = self.transform.backward(img)
+
+        out_img = cv2.addWeighted(out_img, 1, img, 0.6, 0)
+        out_img = self.lanelines.plot(out_img)
+        return out_img
+    
+    def birds_eye(self, img):
+        out_img = np.copy(img)
+        out_img = self.transform.forward(out_img)
+        img = self.transform.forward(img)
+        img = self.thresholding.forward(img)
+        img = self.lanelines.draw_lanes(img)
+
+        out_img = cv2.addWeighted(out_img, 1, img, 0.6, 0)
+        return out_img
+
+    def process_image(self, input_path):
+        img = mpimg.imread(input_path)
+        out_img = self.complete(img)
+        mpimg.imsave(output_file_path(input_path, "_complete", "output_images"), out_img)
+
+    def process_image_all(self, input_path):
+        img = mpimg.imread(input_path)
+        complete_img = self.complete(img)
+        mpimg.imsave(output_file_path(input_path, "_complete", "output_images"), complete_img)
+        sliding_window_img = self.sliding_window(img)
+        mpimg.imsave(output_file_path(input_path, "_sliding_window", "output_images"), sliding_window_img)
+        birds_eye_img = self.birds_eye(img)
+        mpimg.imsave(output_file_path(input_path, "_birds_eye", "output_images"), birds_eye_img)
+
+    def process_video(self, input_path):
+        clip = VideoFileClip(input_path)
+        out_clip = clip.fl_image(self.complete)
+        out_clip.write_videofile(output_file_path(input_path, "_complete", "output_videos"), audio=False)
+
+    def process_video_all(self, input_path):
+        clip = VideoFileClip(input_path)
+        complete_clip = clip.fl_image(self.complete)
+        complete_clip.write_videofile(output_file_path(input_path, "_complete", "output_videos"), audio=False)
+        sliding_window_clip = clip.fl_image(self.sliding_window)
+        sliding_window_clip.write_videofile(output_file_path(input_path, "_sliding_window", "output_videos"), audio=False)
+        birds_eye_clip = clip.fl_image(self.birds_eye)
+        birds_eye_clip.write_videofile(output_file_path(input_path, "_birds_eye", "output_videos"), audio=False)
+
+def main():
+    args = docopt(__doc__)
+    input = args['INPUT_PATH']
+
+    findLaneLines = FindLaneLines()
+    if args['--video'] and args['--all']:
+        findLaneLines.process_video_all(input)
+    elif args['--image'] and args['--all']:
+        findLaneLines.process_image_all(input)
+    elif args['--video']:
+        findLaneLines.process_video(input)
+    elif args['--image']:
+        findLaneLines.process_image(input)
+
+def output_file_path(input_path, suffix, directory):
+    base_name, ext = os.path.splitext(os.path.basename(input_path))
+    new_directory = os.path.join(directory, base_name)
+    os.makedirs(new_directory, exist_ok=True)
+    new_file_name = f"{base_name}{suffix}{ext}"
+    return os.path.join(new_directory, new_file_name)
+
+if __name__ == "__main__":
+    main()
